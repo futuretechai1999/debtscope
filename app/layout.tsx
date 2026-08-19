58da710cdc55f60d59801a68d7250f82a84166f7
Future Tech <futuretechai1999@gmail.com>
2026-08-20 00:22:54 +0530

    feat: setup analytics and update configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import './globals.css'
 import Navbar from '../components/Navbar'
@@ -24,3 +25,4 @@ export default function RootLayout({
     </html>
   )
 }
+<Analytics />
